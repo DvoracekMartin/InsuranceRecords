5a0e4db1cce0e146e00becaa3695fa2449d72c2b
Martin Dvořáček <dvoracekm6@gmail.com>
2022-05-24 15:49:59 +0200

    Added Identity Manage

--- InsuranceRecordsWeb/Program.cs
+++ InsuranceRecordsWeb/Program.cs
@@ -5,6 +5,10 @@ using InsuranceRecordsWeb.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+
 builder.Services.AddDbContext<InsuranceRecordsWeb.Areas.Identity.Data.ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));;
 
