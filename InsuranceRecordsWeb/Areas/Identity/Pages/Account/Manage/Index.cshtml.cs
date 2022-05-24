05eca485673b5a51e7d05ba4e69be00fd6e58852
Martin Dvořáček <dvoracekm6@gmail.com>
2022-05-24 16:38:09 +0200

    Added User Info Edit

--- InsuranceRecordsWeb/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ InsuranceRecordsWeb/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -56,21 +56,56 @@ namespace InsuranceRecordsWeb.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Phone]
+           /* [Phone]
             [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            public string PhoneNumber { get; set; }*/
+
+           
+            [Display(Name = "Jméno")]
+            public string Name { get; set; }
+            
+            [Display(Name = "Příjmení")]
+            public string LastName { get; set; }
+           
+            [Display(Name = "Email/Uživatelské jméno")]
+            public string Email { get; set; }
+           
+            [Display(Name = "Telefonní číslo")]
+            public string TelephoneNumber { get; set; }
+          
+            [Display(Name = "Ulice")]
+            public string StreetName { get; set; }
+            
+            [Display(Name = "Číslo popisné")]
+            public string BuildingNumber { get; set; }
+           
+            [Display(Name = "Město")]
+            public string CityName { get; set; }
+            
+            [Display(Name = "PSČ")]
+            public string ZipCode { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
+            /*var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
-            Username = userName;
+            Username = userName;*/
+           
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                //PhoneNumber = phoneNumber
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                TelephoneNumber = user.TelephoneNumber,
+                StreetName = user.StreetName,
+                BuildingNumber = user.BuildingNumber,
+                CityName = user.CityName,
+                ZipCode = user.ZipCode,
+                
             };
         }
 
@@ -100,8 +135,16 @@ namespace InsuranceRecordsWeb.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            //var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var name = user.Name;
+            var lastName = user.LastName;
+            var email = user.Email;
+            var telephoneNumber = user.TelephoneNumber;
+            var streetName = user.StreetName;
+            var buildingNumber = user.BuildingNumber;
+            var cityName = user.CityName;
+            var zipCode = user.ZipCode;
+            /*if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
                 if (!setPhoneResult.Succeeded)
@@ -109,7 +152,56 @@ namespace InsuranceRecordsWeb.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }*/
+            if (Input.Name != name)
+            {
+                user.Name = Input.Name;
+                await _userManager.UpdateAsync(user);
             }
+            
+            if (Input.LastName != lastName)
+            {
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
+           
+            
+            if (Input.Email != email)
+            {
+                user.Email = Input.Email;
+                await _userManager.UpdateAsync(user);
+            }
+            
+            if (Input.TelephoneNumber != telephoneNumber)
+            {
+                user.TelephoneNumber = Input.TelephoneNumber;
+                await _userManager.UpdateAsync(user);
+            }
+            
+            if (Input.StreetName != streetName)
+            {
+                user.StreetName = Input.StreetName;
+                await _userManager.UpdateAsync(user);
+            }
+            
+            if (Input.BuildingNumber != buildingNumber)
+            {
+                user.BuildingNumber = Input.BuildingNumber;
+                await _userManager.UpdateAsync(user);
+            }
+           
+            if (Input.CityName != cityName)
+            {
+                user.CityName = Input.CityName;
+                await _userManager.UpdateAsync(user);
+            }
+           
+            if (Input.ZipCode != zipCode)
+            {
+                user.ZipCode = Input.ZipCode;
+                await _userManager.UpdateAsync(user);
+            }
+           
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
